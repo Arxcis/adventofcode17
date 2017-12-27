0ca3a710807ee0b0c0d1a440c89c25fa3786b3bc
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2017-12-27 23:43:18 +0100

    day03

--- day03.py
+++ day03.py
@@ -1,19 +1,23 @@
 #!python3
 # -*- coding: utf-8 -*-
 
+#
 # @author Jonas J. Solsvik
 # @date 27.11.17
 # @url https://adventofcode.com/2017/day/2
-
+#
 
 if __name__ == "__main__":
 
     # 0. Initial state
-    target = 50
-    step   = 1
-    value  = 0
-    offset = -1    
+    target  = 50
+    step    = 1
+    value   = 0
+    offset  = -1
+    lap     = 0
     running = True
+    right_turns        = 0
+    manhattan_distance = 0
 
     # 1. Advance iterator to hit spiral target
     print("val", 
@@ -21,36 +25,47 @@ if __name__ == "__main__":
           "\t","mid", 
           "\t","off",
           "\t", "offs", 
-          "\t", "mdist")
+          "\t", "mdist",
+          "\t", "rturn", 
+          "\t", "lap")
+
     while running:
-        for i in range(step*2):
-            if value < target:
-                value += 1
-                offset += 1
-                offset_step = offset % step
-                mid = step // 2
+        for i in range(step * 2):
 
-                # 
-                # MANHATTAN DISTANCE
-                #
-                distance_center = 0
-                distance_mid    = abs(offset_step - mid)
-                
-
-                manhattan_distance = mid + distance_mid
-
-                print(value, 
-                      "\t",step, 
-                      "\t",mid, 
-                      "\t",offset,
-                      "\t", offset_step)
-                      #\t", manhattan_distance)
-            else:
+            # STOP - if we have found the target
+            if value >= target:
                 running = False
                 break
 
-        step += 1
-        offset = -1
+            value  += 1
+            offset += 1
+            offset_step = offset % step
+            mid = step // 2
 
-    # 2. Calculate Manhattan Distance(steps) to spiral center
-    
+            # If right turn
+            if offset_step == 0: 
+                right_turns += 1
+                #print("TURNING!")
+
+                # If new lap (every 4th right turn)
+                if (right_turns + 2) % 4 == 0:
+                    lap += 1
+                    #print("LAPPING!")
+
+            distance_mid       = abs(offset_step - mid)
+            distance_center    = lap
+            manhattan_distance = distance_mid + distance_center
+
+            '''
+            print(value, 
+                  "\t",step, 
+                  "\t",mid, 
+                  "\t",offset,
+                  "\t", offset_step,
+                  "\t", manhattan_distance,
+                  "\t", right_turns,
+                  "\t", lap)'''
+
+        #print("STEP UP!")
+        step   +=  1
+        offset  = -1
